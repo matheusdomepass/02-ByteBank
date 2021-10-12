3aeba726d30b43ccfd5197400333c9011ce32447
Matheus Passatuto <50487734+matheusdomepass@users.noreply.github.com>
2021-10-12 11:14:30 -0300

    Add files via upload

---
+++ ByteBank/Funcionarios/Sistema/Autenticavel.cs
@@ -0,0 +1,15 @@
+﻿using ByteBank.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Funcionarios.Sistema
+{
+    public interface Autenticavel
+    {
+        bool Autenticar(string senha);
+        
+    }
+}
